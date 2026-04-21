0328daf91117d1d22f162485845f0efeeae25735
SiddharthaManthurthi <manthurthisiddhartha@gmail.com>
2026-04-21 11:23:38 +0530

    k8

--- 00-vpc/main.tf
+++
@@ -1,6 +0,0 @@
-module "vpc" {
-    source = "git::https://github.com/SiddharthaManthurthi/terraform-aws-vpc.git?ref=main"   #before subnets creation we can use folder path after we can use git
-    project = var.project
-    environment = var.environment
-    is_peering_required = true
-}
